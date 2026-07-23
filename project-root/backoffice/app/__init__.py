db261b5f0b92c4b93cd498c501a4cb42a908718b
Souf-F <sfecom.31000@gmail.com>
2026-07-23 17:02:43 +0200

    fix(cors): update CORS origins to include additional localhost ports

--- project-root/backoffice/app/__init__.py
+++ project-root/backoffice/app/__init__.py
@@ -16,7 +16,12 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    CORS(app, supports_credentials=True, origins=["http://127.0.0.1:5500", "http://localhost:5500"])
+    CORS(app, supports_credentials=True, origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
+    ])
 
     db.init_app(app)
 
